11ccf205e621e37e11906b8192ca0524212771b9
William Multani <william.multani@gmail.com>
2022-11-23 14:19:17 +0100

    Story 6A:UserController updated

--- src/main/java/com/switchfully/digibooky/api/UserController.java
+++ src/main/java/com/switchfully/digibooky/api/UserController.java
@@ -1,12 +1,27 @@
 package com.switchfully.digibooky.api;
 
+import com.switchfully.digibooky.api.dtos.CreateMemberDto;
+import com.switchfully.digibooky.api.dtos.MemberDto;
+import com.switchfully.digibooky.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "users")
 public class UserController {
+
     private final Logger myLogger = LoggerFactory.getLogger(UserController.class);
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public MemberDto createMember(@RequestBody CreateMemberDto createMemberDto) {
+        return userService.createMember(createMemberDto);
+    }
 }
