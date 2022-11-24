a19f0a44d8c13cf22fcde43635e262fd8b7246b6
Dries Van Hool <driesvanhool@gmail.com>
2022-11-24 13:52:34 +0100

    User repository test

--- src/test/java/com/switchfully/digibooky/domain/repositories/UserRepositoryTest.java
+++ src/test/java/com/switchfully/digibooky/domain/repositories/UserRepositoryTest.java
@@ -30,39 +30,50 @@ class UserRepositoryTest {
             //GIVEN WHEN
             Member savedMember = userRepository.save(memberToSave);
 
-        //THEN
-        Assertions.assertEquals(memberToSave, savedMember);
+            //THEN
+            Assertions.assertEquals(memberToSave, savedMember);
+        }
+
+        @Test
+        void whenANewUserThatAllreadyExists_thenExceptionThrowsIlligalArgument() {
+            //GIVEN WHEN
+            Member savedMember = userRepository.save(memberToSave);
+
+            //THEN
+            Throwable exception = assertThrows(IllegalArgumentException.class, ()->userRepository.save(memberToSave));
+            Assertions.assertEquals(savedMember.getName()+ " " + savedMember.getSurname() + " already exists.", exception.getMessage());
+        }
+
+        @Test
+        void whenAEmailAllreadyExists_thenExceptionThrowsIlligalArgument() {
+            //GIVEN WHEN
+            Member savedMember = userRepository.save(memberToSave);
+            Member memberEmail = new Member("Emailnaam", "Tester", "test@gmail.com", Role.MEMBER, "pwd",  "90.03.01-997-05", "Kurtstraaat", "25", new City("2000", "Antwerpen"));
+
+            //THEN
+            Throwable exception = assertThrows(IllegalArgumentException.class, ()->userRepository.save(memberEmail));
+            Assertions.assertEquals("This email adress already exists.", exception.getMessage());
+        }
+
+        @Test
+        void whenAInssAllreadyExists_thenExceptionThrowsIlligalArgument() {
+            //GIVEN WHEN
+            Member savedMember = userRepository.save(memberToSave);
+            Member memberInss = new Member("Emailnaam", "Tester", "Emailnaam@gmail.com", Role.MEMBER, "pwd",  "90.03.01-997-04", "Kurtstraaat", "25", new City("2000", "Antwerpen"));
+
+            //THEN
+            Throwable exception = assertThrows(IllegalArgumentException.class, ()->userRepository.save(memberInss));
+            Assertions.assertEquals("This inss already exists.", exception.getMessage());
+        }
     }
 
-    @Test
-    void whenANewUserThatAllreadyExists_thenExceptionThrowsIlligalArgument() {
-        //GIVEN WHEN
-        Member savedMember = userRepository.save(memberToSave);
-
-        //THEN
-        Throwable exception = assertThrows(IllegalArgumentException.class, ()->userRepository.save(memberToSave));
-        Assertions.assertEquals(savedMember.getName()+ " " + savedMember.getSurname() + " already exists.", exception.getMessage());
+    @Nested
+    @DisplayName("User tests")
+    public class UserTests {
+        @BeforeEach
+        void initTestData(){
+            userToSave = new User("Test", "Tester", "test@gmail.com", Role.MEMBER, "pwd");
+        }
     }
 
-    @Test
-    void whenAEmailAllreadyExists_thenExceptionThrowsIlligalArgument() {
-        //GIVEN WHEN
-        Member savedMember = userRepository.save(memberToSave);
-        Member memberEmail = new Member("Emailnaam", "Tester", "test@gmail.com", Role.MEMBER, "pwd",  "90.03.01-997-05", "Kurtstraaat", "25", new City("2000", "Antwerpen"));
-
-        //THEN
-        Throwable exception = assertThrows(IllegalArgumentException.class, ()->userRepository.save(memberEmail));
-        Assertions.assertEquals("This email adress already exists.", exception.getMessage());
-    }
-
-    @Test
-    void whenAInssAllreadyExists_thenExceptionThrowsIlligalArgument() {
-        //GIVEN WHEN
-        Member savedMember = userRepository.save(memberToSave);
-        Member memberInss = new Member("Emailnaam", "Tester", "Emailnaam@gmail.com", Role.MEMBER, "pwd",  "90.03.01-997-04", "Kurtstraaat", "25", new City("2000", "Antwerpen"));
-
-        //THEN
-        Throwable exception = assertThrows(IllegalArgumentException.class, ()->userRepository.save(memberInss));
-        Assertions.assertEquals("This inss already exists.", exception.getMessage());
-    }
 }
