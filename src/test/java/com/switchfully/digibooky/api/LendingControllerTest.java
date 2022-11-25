6fb95648d2d2e5d40f9f4dd979b878b445536297
Casper Caluwe <casper.c@live.be>
2022-11-25 17:04:02 +0100

    list lend books for member finished

--- src/test/java/com/switchfully/digibooky/api/LendingControllerTest.java
+++ src/test/java/com/switchfully/digibooky/api/LendingControllerTest.java
@@ -5,20 +5,22 @@ import com.switchfully.digibooky.api.dtos.ReturnLibraryItemDto;
 import com.switchfully.digibooky.domain.Author;
 import com.switchfully.digibooky.domain.Book;
 import com.switchfully.digibooky.domain.LendItem;
+import com.switchfully.digibooky.domain.*;
 import com.switchfully.digibooky.domain.repositories.BookRepository;
 import com.switchfully.digibooky.domain.repositories.LendingRepository;
+import com.switchfully.digibooky.domain.repositories.LendingRepository;
+import com.switchfully.digibooky.domain.repositories.UserRepository;
+import com.switchfully.digibooky.domain.security.Role;
 import io.restassured.RestAssured;
 import io.restassured.common.mapper.TypeRef;
 import net.minidev.json.JSONObject;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -47,6 +49,48 @@ class LendingControllerTest {
        testMemberId = "1";
        testMemberPw = "pwd";
    }
+    @Test
+    void givenAnAuthorizedUser_whenLendingABook_thenBookIdEquals() {
+        LendItemDto result =
+                RestAssured.given().port(port).auth().preemptive().basic("1", "pwd").log().all().contentType("application/json")
+                        .with().queryParam("isbn", "100")
+                        .when().post("lending/book")
+                        .then().statusCode(201).and().extract().body().as(LendItemDto.class);
+
+        assertEquals(testBook.getId(), result.itemId());
+    }
+
+    @DisplayName("Authorization test")
+    @Nested
+    class AuthorizationCheck{
+        @Test
+        void givenAUnKnownUser_whenLendingABook_thenThrowsUnknownUserException() {
+            Map<String, String> response =
+                    RestAssured.given().port(port).auth().preemptive().basic("0", "pwd").log().all().contentType("application/json")
+                            .with().queryParam("isbn", "100")
+                            .when().post("lending/book")
+                            .then().statusCode(403).and().extract().body().as(new TypeRef<Map<String, String>>() {
+                            });
+
+            String ResponseMessage = new JSONObject(response).get("message").toString();
+
+            assertEquals("Unauthorized", ResponseMessage);
+        }
+        @Test
+        void givenAWrongPassword_whenLendingABook_thenThrowsUnknownUserException() {
+            Map<String, String> response =
+                    RestAssured.given().port(port).auth().preemptive().basic("1", "x").log().all().contentType("application/json")
+                            .with().queryParam("isbn", "100")
+                            .when().post("lending/book")
+                            .then().statusCode(403).and().extract().body().as(new TypeRef<Map<String, String>>() {
+                            });
+
+            String ResponseMessage = new JSONObject(response).get("message").toString();
+
+            assertEquals("Unauthorized", ResponseMessage);
+        }
+    }
+
 
 
     @DisplayName("return test")
@@ -114,112 +158,6 @@ class LendingControllerTest {
 
             String ResponseMessage = new JSONObject(result).get("message").toString();
             assertEquals("Unauthorized", ResponseMessage);
-        }
-    }
-
-    @Test
-    void givenAnAuthorizedUser_whenLendingABook_thenBookIdEquals() {
-        LendItemDto result =
-                RestAssured.given().port(port).auth().preemptive().basic("1", "pwd").log().all().contentType("application/json")
-                        .with().queryParam("isbn", "100")
-                        .when().post("lending/book")
-                        .then().statusCode(201).and().extract().body().as(LendItemDto.class);
-
-        assertEquals(testBook.getId(), result.itemId());
-    }
-
-    @DisplayName("Authorization test")
-    @Nested
-    class AuthorizationCheck{
-        @Test
-        void givenAUnKnownUser_whenLendingABook_thenThrowsUnknownUserException() {
-            Map<String, String> response =
-                    RestAssured.given().port(port).auth().preemptive().basic("0", "pwd").log().all().contentType("application/json")
-                            .with().queryParam("isbn", "100")
-                            .when().post("lending/book")
-                            .then().statusCode(403).and().extract().body().as(new TypeRef<Map<String, String>>() {
-                            });
-
-            String ResponseMessage = new JSONObject(response).get("message").toString();
-
-            assertEquals("Unauthorized", ResponseMessage);
-        }
-        @Test
-        void givenAWrongPassword_whenLendingABook_thenThrowsUnknownUserException() {
-            Map<String, String> response =
-                    RestAssured.given().port(port).auth().preemptive().basic("1", "x").log().all().contentType("application/json")
-                            .with().queryParam("isbn", "100")
-                            .when().post("lending/book")
-                            .then().statusCode(403).and().extract().body().as(new TypeRef<Map<String, String>>() {
-                            });
-
-            String ResponseMessage = new JSONObject(response).get("message").toString();
-
-            assertEquals("Unauthorized", ResponseMessage);
-        }
-    }
-
-    @DisplayName("test with duplicate books/loans")
-    @Nested
-    public class MultipleBookTests {
-       @Test
-        void givenTwoTestBooks_BothAvailable_Return1() {
-           Book testBook2 = new Book("testbook", "a book for testing", "100", new Author("test", "McTest"));
-           bookRepository.createBook(testBook2);
-
-           LendItemDto result =
-                   RestAssured.given().port(port).auth().preemptive().basic("1", "pwd").log().all().contentType("application/json")
-                           .with().queryParam("isbn", "100")
-                           .when().post("lending/book")
-                           .then().statusCode(201).and().extract().body().as(LendItemDto.class);
-
-           assertEquals(testBook.getId(), result.itemId());
-       }
-
-       @Test
-        void givenTwoTestBooks_OneAlreadyLoanedOut_LendOtherBook() {
-           Book testBookUnique = new Book("testbookU", "a book for testing", "101", new Author("test", "McTest"));
-           bookRepository.createBook(testBookUnique);
-           Book testBookUnique2 = new Book("testbookU", "a book for testing", "101", new Author("test", "McTest"));
-           bookRepository.createBook(testBookUnique2);
-           testBookUnique.setIsLended(true);
-
-           LendItemDto result =
-                   RestAssured.given().port(port).auth().preemptive().basic("1", "pwd").log().all().contentType("application/json")
-                           .with().queryParam("isbn", "101")
-                           .when().post("lending/book")
-                           .then().statusCode(201).and().extract().body().as(LendItemDto.class);
-
-           assertEquals(testBookUnique2.getId(), result.itemId());
-
-       }
-      @Test
-        void givenLoanedOutBook_WhenTryingToLoan_ThrowsNoSuchElementExc() {
-           testBook.setIsLended(true);
-           Map<String, String> result =
-                   RestAssured.given().port(port).auth().preemptive().basic("1", "pwd").log().all().contentType("application/json")
-                           .with().queryParam("isbn", "100")
-                           .when().post("lending/book")
-                           .then().statusCode(404).and().extract().
-                            body().as(new TypeRef<Map<String, String>>() {
-                   });
-
-           String ResponseMessage = new JSONObject(result).get("message").toString();
-           assertEquals("Book with isbn: 100 is not available", ResponseMessage);
-       }
-       @Test
-        void givenDeletedBook_WhenTryingToLoan_ThrowsNoSuchElementExc() {
-            testBook.setDeleted(true);
-            Map<String, String> result =
-                    RestAssured.given().port(port).auth().preemptive().basic("1", "pwd").log().all().contentType("application/json")
-                            .with().queryParam("isbn", "100")
-                            .when().post("lending/book")
-                            .then().statusCode(404).and().extract().
-                            body().as(new TypeRef<Map<String, String>>() {
-                            });
-
-            String ResponseMessage = new JSONObject(result).get("message").toString();
-            assertEquals("Book with isbn: 100 is not available", ResponseMessage);
         }
     }
 }
